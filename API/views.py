2ae22164f84863fb78142a170288080651eca7b3
Nayan Das <ndas5144@gmail.com>
2022-10-08 13:02:16 +0530

    updated project

--- API/views.py
+++ API/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from scrape import scrape
 
-# Create your views here.
+
+class GetTweets(generics.ListAPIView):
+    def get(self, *args, **kwargs):
+        get_data = self.request.query_params
+        query = get_data['search']
+        if get_data.get('wfh') and get_data.get('wfh') == 'true':
+            query = query+' remote'
+            
+        resp= scrape(
+            query=query,
+            start_date=get_data['start_date'],
+            end_date=get_data['end_date']
+            )
+        return Response(data = resp)
---
+++ scrape.py
@@ -0,0 +1,59 @@
+import snscrape.modules.twitter as twitterScraper
+import json
+
+job_board_list = [
+    "RemoteTechJobs0", 
+    "zobjobsUS", 
+    "RemoteSonar", 
+    "RecruitngEdge",
+    "CodingJobsIt",
+    "jobscanCo",
+    "JobHuntOrg",
+    "justremoteco",
+    "nodeskco",
+    "weworkremotely",
+    "jobmote",
+    "WantRemoteJob",
+    "daily_remote",
+    "remoteworkhub",
+    "workingnomads",
+    "DevStartupJobs",
+    "zobjobsGB",
+    "growremotelyio",
+    "jobsincrypto",
+    "Rezoomex",
+    "_remotify",
+    "Up2staff",
+    "zobjobsCA",
+]
+
+block_list = ["https://twitter.com/" + e for e in job_board_list]
+
+
+def scrape(**kwargs):
+    # query = f'{kwargs["query"]} since:2022-09-01 until:2022-09-31'
+    query = f'{kwargs["query"]} since:{kwargs["start_date"]} until:{kwargs["end_date"]}'
+    scraper = twitterScraper.TwitterSearchScraper(query)
+    tweets = []
+    max_count = 20
+    for i, tweet in enumerate(scraper.get_items()):
+        if i == (max_count):
+            break
+
+        if str(tweet.user) not in block_list:
+            bodies = tweet.content
+            # bodies = ''.join(' ' + val.replace('\xa0', '').replace('\r\n', '').strip() for val in bodies)
+            tweets.append({
+                "id": str(tweet.id),
+                "url": tweet.url,
+                "content": bodies,
+                "date": str(tweet.date)
+            })
+        else:
+            max_count += 1
+    return tweets
+
+
+if __name__ == "__main__":
+    # scrape("develoepr job remote")
+    print(scrape('django'))
